a2cee0ef5a98381b2c69c9e26a4b60c5f6d29edb
JS <zks145@naver.com>
2025-11-18 21:12:24 +0900

    feat: api 데이터 추출을 위한 dto 생성

---
+++ lib/MosData.dart
@@ -0,0 +1,13 @@
+class MosData {
+  final String date;
+  final double valueWater;
+  final double valueHouse;
+  final double valuePark;
+
+  MosData({
+    required this.date,
+    required this.valueWater,
+    required this.valueHouse,
+    required this.valuePark
+  });
+}
